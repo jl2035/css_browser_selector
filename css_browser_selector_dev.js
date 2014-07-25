11a3bcd7ec988f6d1c6fd3735bab76f34a0df0ba
jl2035 <jaka.luthar@gmail.com>
2014-07-25 17:49:32 +0200

    Updated the dev version

--- css_browser_selector_dev.js
+++ css_browser_selector_dev.js
@@ -40,7 +40,8 @@ function css_browser_selector(u)
 		b=	[
 		
 			// browser
-			(!(/opera|webtv/i.test(ua))&&(/msie\s(\d)/.test(ua)||/trident/.test(ua)))?('ie ie'+(/trident\/4\.0/.test(ua) ? '8' : RegExp.$1))
+			(!(/opera|webtv/i.test(ua))&&!(/msie\s(\d+)/.test(ua))&&(/trident/.test(ua)))?'ie11':
+			(!(/opera|webtv/i.test(ua))&&/msie\s(\d+)/.test(ua))?('ie ie'+(/trident\/4\.0/.test(ua) ? '8' : RegExp.$1))
 			:is('firefox/')?g+ " " + f+(/firefox\/((\d+)(\.(\d+))(\.\d+)*)/.test(ua)?' '+f+RegExp.$2 + ' '+f+RegExp.$2+"_"+RegExp.$4:'')	
 			:is('gecko/')?g
 			:is('opera')?o+(/version\/((\d+)(\.(\d+))(\.\d+)*)/.test(ua)?' '+o+RegExp.$2 + ' '+o+RegExp.$2+"_"+RegExp.$4 : (/opera(\s|\/)(\d+)\.(\d+)/.test(ua)?' '+o+RegExp.$2+" "+o+RegExp.$2+"_"+RegExp.$3:''))
